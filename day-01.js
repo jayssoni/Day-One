9114695c83e3c8c1c5fc266994738abcb9fe85e5
nisha-saw <sawnisha767@gmail.com>
2025-06-12 22:02:57 +0530

    fifth commit

--- day-01.js
+++ day-01.js
@@ -1,2 +1,24 @@
 const value= 1;
 console.log(value);
+
+//NISHA
+
+let arr = [1, 4, 5, 6, 0, 0, 9, 0];
+
+let pos = 0; // Position to place the next non-zero
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] !== 0) {
+    // Swap arr[i] with arr[pos] if i != pos
+    if (i !== pos) {
+      let temp = arr[i];
+      arr[i] = arr[pos];
+      arr[pos] = temp;
+    }
+    pos++;
+  }
+}
+
+console.log(arr);  // [1, 4, 5, 6, 9, 0, 0, 0]
+
+
