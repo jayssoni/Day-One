03c5ff70b915e98aa362d41d9459c26258d9551a
Deepanshu-Sahu <deepanshusahu720@gmail.com>
2025-06-14 10:57:50 +0530

    commit by deepanshu

--- day-01.js
+++ day-01.js
@@ -1,2 +1,14 @@
 const value= 1;
 console.log(value);
+console.log(value + 1); 
+
+
+
+// this is a code from deepanshu 
+//ever or odd
+const number = 2;
+if (number % 2 === 0) {
+    console.log("Even");
+}else {
+    console.log("Odd");
+}
